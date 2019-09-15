8b054c57b5e4efba0c156c4cc978b5f4494fd048
Takuya Shinjo <e175769@ie.u-ryukyu.ac.jp>
2019-09-15 19:50:33 +0900

    add GA code

---
+++ ga.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+//パラメータ
+var gene_length int = 10       //遺伝子の長さ
+var individual_length int = 10 //個体数
+var generation int = 20        // 世代数
+var mutate_rate float32 = 0.1  //突然変異の確率
+var elite_rate float32 = 0.2   //エリートを選択する割合
+
+func main() {
+	fmt.Println("Hello World!!	")
+}
